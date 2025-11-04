7a513ae2686c81df48abd396040649504d17b605
Priyanshu Garg <39775480+priyanshugarg29@users.noreply.github.com>
2025-11-04 22:36:53 +0530

    Create apps.py

---
+++ apps.py
@@ -0,0 +1,8 @@
+# Importing libraries
+
+import streamlit as st
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+API_BASE_URL = "https://stock-nse-india-api-url"  # API base URL
